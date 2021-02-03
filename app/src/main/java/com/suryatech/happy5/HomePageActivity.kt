48deb468c5dc2cb08871fd752408e58dfdc20639
adiisry <adiisry.00@gmail.com>
2021-02-03 15:36:53 +0700

    Front-End 1

---
+++ app/src/main/java/com/suryatech/happy5/HomePageActivity.kt
@@ -0,0 +1,11 @@
+package com.suryatech.happy5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
